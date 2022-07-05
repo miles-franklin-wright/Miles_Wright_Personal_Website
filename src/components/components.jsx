d8c8107f3288b3154b9e15b2c3936e610059e2d6
miles-franklin <67435684+miles-franklin-wright@users.noreply.github.com>
2022-07-05 23:55:59 +0000

    restarting and simplifying

--- src/components/components.jsx
+++ src/components/components.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
-import Miles from './miles/miles.jsx';
-import Find from './find/find.jsx';
-import Work from './work/work.jsx';
-import Pattern from './patterns/pattern.jsx';
+
 
 
 function Components() {
   return (
     <div>
-      <Miles/>
-      <Find/>
-      <Work/>
-      <Pattern/>
+      Components
     </div>
   );
 }
