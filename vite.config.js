d79180ee4616374e5db4a3596702ca1a9f1df940
miles-franklin <67435684+miles-franklin-wright@users.noreply.github.com>
2022-07-17 15:43:47 +0000

    Day 3 - attempting to add the dist folder (for the gh pages build) by removing dist and dist-ssr from the gitignore file. I will revert back if this change does not work

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
  * @type { import('vite').UserConfig }
  */
 export default {
+  base: '/Miles_Wright_Personal_Website/',
   plugins: [reactRefresh()],
   server: {
     host: '0.0.0.0',
